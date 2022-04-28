eeb87e1081ac18e18b051a04713d0f357e599154
Luca A <lucale1114@skola.goteborg.se>
2022-04-28 12:04:18 +0200

    Setup completed

---
+++ open-library-vue/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+<h1>Hi</h1>
+<h1>Greetings</h1>
+<h1>Hello</h1>
+  <header>
+    <div class="wrapper">
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+
+</style>
---
+++ open-library-vue/src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+  <h1>HEADER!</h1>
+  </main>
+</template>
