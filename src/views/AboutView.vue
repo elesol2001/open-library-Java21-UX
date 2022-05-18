f6b9dd54e59a0948e3cb14582bd5d800529b4597
gabqui <gabqui0405@skola.goteborg.se>
2022-05-18 11:42:23 +0200

    Changed stuff

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,8 @@
   <div class="about">
     <h1>{{ thing }}</h1>
     <img :src="imageUrl" alt="" />
+
+
   </div>
 </template>
 
@@ -14,9 +16,6 @@ export default {
       return `https://covers.openlibrary.org/b/olid/${this.thing}-S.jpg`;
       // 0450032205-M
     },
-    getTitle() {
-      return `https://openlibrary.org/works/${this.thing.name}.json`;
-    }
   },
 };
 </script>
