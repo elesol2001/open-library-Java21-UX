f39951a5398996c1bc83c1faed0402a8de24a1da
gabqui <gabqui0405@skola.goteborg.se>
2022-05-19 10:20:36 +0200

    Real commit

--- src/router/index.js
+++ src/router/index.js
@@ -34,39 +34,6 @@ const router = createRouter({
             component: SearchIndex
         }
     ],
-=======
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about/:thing",
-      name: "about",
-      props: true,
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
-    },
-    {
-      path: "/title",
-      name: "title",
-      component: () => import("../views/TitleView.vue"),
-    },
-    {
-      path: "/mylib",
-      name: "mylib",
-      component: () => import("../MyLibrary.vue"),
-    },
-    {
-      path: "/searchIndex",
-      name: "SearchIndex",
-      component: SearchIndex,
-    },
-  ],
 })
 
 export default router
