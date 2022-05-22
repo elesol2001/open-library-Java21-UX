df065639a3a870cd9ab01deaae43b1f2cec2b0e2
Alexander Karlsson Göransson <alexanderkarlssongoransson@hotmail.com>
2022-05-22 18:21:05 +0200

    Added equivalent add book button, but for marking a book as read or not

---
+++ src/components/IsBookRead.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="toggleIsBookRead" v-if="isBookRead">
+    <img src="@/assets/marked-as-read-icon.svg" alt="Book is toggled as read" />
+  </button>
+  <button @click="toggleIsBookRead" v-else>
+    <img src="@/assets/mark-as-read-icon.svg" alt="Book is not toggled as read" />
+  </button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isBookRead: false,
+    }
+  },
+  methods: {
+    toggleIsBookRead() {
+      this.isBookRead = !this.isBookRead
+    },
+  },
+}
+</script>
+
+<style></style>
