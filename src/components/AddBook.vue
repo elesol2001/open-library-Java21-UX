ab316bb3d1a9b750d737418ef62a4322ab079d08
Alexander Karlsson Göransson <alexanderkarlssongoransson@hotmail.com>
2022-05-22 17:29:04 +0200

    Added toggleable icon for having read a book or not. Put it in SearchIndex for now just to see it visually on the screen.

---
+++ src/components/AddBook.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="addBook" v-if="isBookAdded">
+    <img src="@/assets/added-book-icon.svg" alt="Book has been added" />
+  </button>
+  <button @click="addBook" v-else>
+    <img src="@/assets/add-book-icon.svg" alt="Add book" />
+  </button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isBookAdded: false,
+    }
+  },
+  methods: {
+    addBook() {
+      this.isBookAdded = !this.isBookAdded
+    },
+  },
+}
+</script>
+
+<style></style>
